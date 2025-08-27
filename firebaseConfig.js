b8d505fb76b9ef64a5ceb3e6d9ed9eb049b2cda6
gessylbriones <gessylmaebriones16@gmail.com>
2025-08-27 15:34:14 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUAeIWFRJxsJE2nrF8kYyQg8j6r5xau3I",
+  authDomain: "ja-goal.firebaseapp.com",
+  projectId: "ja-goal",
+  storageBucket: "ja-goal.firebasestorage.app",
+  messagingSenderId: "221176156822",
+  appId: "1:221176156822:web:cdc65ef792125541d3e8b7"
+};
+
+const app = initializeApp(firebaseConfig);
